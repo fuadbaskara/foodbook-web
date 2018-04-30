cd4fe90d7cc568a648c5ef6618d6591e9c68c70e
stefanus dody <stefanusdody@yahoo.com>
2018-04-30 16:17:23 +0700

    temporarydesign

---
+++ src/page/login.jsx
@@ -0,0 +1,9 @@
+import React, { Component} from "react";
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Link, Route} from "react-router-dom";
+
+const Login = () => (
+    <h1>HELLO LOGIN</h1>
+    )
+
+export default Login
