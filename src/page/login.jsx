0e7d575f5e10c41d61379b841df0342b541df7b2
Miftah Faris <miftah.faris07@yahoo.co.id>
2018-05-02 10:40:46 +0700

    modified form login

--- src/page/login.jsx
+++ src/page/login.jsx
@@ -1,9 +1,51 @@
-import React, { Component} from "react";
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
-import { Link, Route} from "react-router-dom";
+import React from "react";
+import { Button } from "reactstrap";
 
-const Login = () => (
-    <h1>HELLO LOGIN</h1>
-    )
+const divStyle = {
+  padding: "50px",
+  border: "solid lightskyblue 2px",
+  width: "40%",
+  marginLeft: "auto",
+  marginRight: "auto",
+  borderRadius: "5px",
+  backgroundColor: "red"
+};
 
-export default Login
+const fontStyle = {
+  font: "bold 18px arial, sans-serif",
+  padding: "20px"
+};
+
+const buttonStyle = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center"
+};
+
+const inputStyle = {
+  border: "solid black 2px"
+};
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div>
+        <form style={divStyle}>
+          <div>
+            <label style={fontStyle}>USERNAME :</label>
+            <input type="username" style={inputStyle} />
+          </div>
+          <div>
+            <label style={fontStyle}>PASSWORD :</label>
+            <input type="password" style={inputStyle} />
+          </div>
+          <div style={buttonStyle}>
+            <Button color="secondary">LOGIN</Button>{" "}
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
