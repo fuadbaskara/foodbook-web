a52b771ef8ab32286d86660a5c98463ef9329fe5
stefanus dody <stefanusdody@yahoo.com>
2018-05-03 10:36:59 +0700

    new addfood and login

--- src/page/addfood.jsx
+++ src/page/addfood.jsx
@@ -16,78 +16,165 @@ import {
   NavItem,
   NavLink
  } from 'reactstrap';
-import { Link, Route} from "react-router-dom";
+import {
+  Link,
+  Route} from "react-router-dom";
 
-import Home from "./home";
 
-const addFood= ({match}) => (
-    <div>
-      <Form className="addFoodForm">
-       <FormGroup row>
-         <Label for="exampleFood" sm={2}>FOOD NAME</Label>
-         <Col sm={10}>
-           <Input type="foodname" name="foodname" placeholder="insert food name" />
-         </Col>
-      </FormGroup>
-      <FormGroup row>
-         <Label for="exampleAddress" sm={2}>ADDRESS</Label>
-         <Col sm={10}>
-           <Input type="Address" name="Address" placeholder="insert Address" />
-         </Col>
-       </FormGroup>
+class addFood extends Component {
+  constructor(){
+    super();
+    this.state = {
+     inputFoodName: "",
+     inputAddress: "",
+     inputCity: "",
+     inputLocation: "",
+     inputDescriptionMenu: "",
+     inputPrice: "",
+    };
+   this.handleChangeFood = this.handleChangeFood.bind(this);
+   this.handleChangeAddress = this.handleChangeAddress.bind(this);
+   this.handleChangeCity = this.handleChangeCity.bind(this);
+   this.handleChangeLocation = this.handleChangeLocation.bind(this);
+   this.handleChangeDescriptionMenu = this.handleChangeDescriptionMenu.bind(this);
+   this.handleChangePrice = this.handleChangePrice.bind(this);
+  }
 
-       <FormGroup row>
-          <Label for="exampleCity" sm={2}>CITY</Label>
-          <Col sm={10}>
-            <Input type="select" name="select" id="exampleSelect">
-              <option>Jakarta</option>
-              <option>Jakarta</option>
-              <option>Jakarta</option>
-              <option>Jakarta</option>
-              <option>Jakarta</option>
-            </Input>
-          </Col>
-        </FormGroup>
+  handleChangeFood(event){
+    let value = event.target.value;
+    this.setState(() => {
+      return { inputFoodName: value };
+    })
+  }
 
+handleChangeAddress(event){
+  let value = event.target.value;
+  this.setState(() =>{
+    return{ inputAddress: value }
+  })
+}
+
+handleChangeCity(event){
+  let value = event.target.value;
+  this.setState(() =>{
+    return{ inputCity: value }
+  })
+}
+
+handleChangeLocation(event){
+  let value = event.target.value;
+  this.setState(() =>{
+    return{ inputLocation: value }
+  })
+}
+
+handleChangeDescriptionMenu(event){
+  let value = event.target.value;
+  this.setState(() =>{
+    return{ inputDescriptionMenu: value }
+  })
+}
+
+handleChangePrice(event){
+  let value = event.target.value;
+  this.setState(() =>{
+    return{ inputPrice: value }
+  })
+}
+
+submitForm(event){
+  event.preventDefault()
+}
+
+  render(){
+    console.log('state', this.state);
+    return (
+      <div onSubmit={this.submitForm}>
+        <Form className="addFoodForm">
          <FormGroup row>
-           <Label for="exampleLocation" sm={2}>LOCATION</Label>
+           <Label for="exampleFood" sm={2}>FOOD NAME</Label>
            <Col sm={10}>
-             <Input type="Location" name="Location" placeholder="insert your Location" />
+             <Input
+             type="foodname"
+             name="foodname"
+             placeholder="Insert Food Name"
+             value={this.state.inputFoodName}
+             onChange={this.handleChangeFood}/>
+           </Col>
+        </FormGroup>
+        <FormGroup row>
+           <Label for="exampleAddress" sm={2}>ADDRESS</Label>
+           <Col sm={10}>
+             <Input
+             type="Address"
+             name="Address"
+             placeholder="Insert Address"
+             value={this.state.inputAddress}
+             onChange={this.handleChangeAddress}/>
            </Col>
          </FormGroup>
-
          <FormGroup row>
-            <Label for="exampleMenus" sm={2}>MENUS</Label>
+            <Label for="exampleCity" sm={2}>CITY</Label>
             <Col sm={10}>
-              <Input type="select" name="select" id="exampleSelect">
-                <option>MENUS</option>
-                <option>MENUS</option>
-                <option>MENUS</option>
-                <option>MENUS</option>
-                <option>MENUS</option>
-              </Input>
+              <Input
+              type="City"
+              name="City"
+              placeholder="Insert City"
+              value={this.state.inputCity}
+              onChange={this.handleChangeCity}/>
             </Col>
           </FormGroup>
+           <FormGroup row>
+             <Label for="exampleLocation" sm={2}>LOCATION</Label>
+             <Col sm={10}>
+               <Input
+               type="Location"
+               name="Location"
+               placeholder="Insert Your Location"
+               value={this.state.inputLocation}
+               onChange={this.handleChangeLocation}/>
+             </Col>
+           </FormGroup>
+           <FormGroup row>
+              <Label for="exampleMenus" sm={2}>DESCRIPTION MENU</Label>
+              <Col sm={10}>
+                <Input
+                type="Menus"
+                name="Menus"
+                placeholder="Insert Your Menu"
+                value={this.state.inputDescriptionMenu}
+                onChange={this.handleChangeDescriptionMenu}/>
+              </Col>
+            </FormGroup>
+            <FormGroup row>
+              <Label for="examplePrice" sm={2}>PRICE</Label>
+              <Col sm={10}>
+                <Input
+                type="Price"
+                name="Price"
+                placeholder="Insert Price Menu"
+                value={this.state.inputPrice}
+                onChange={this.handleChangePrice}/>
+              </Col>
+            </FormGroup>
+            <FormGroup row>
+              <Label for="examplePhoto" sm={2}>PHOTO</Label>
+              <Col sm={10}>
+              <button id="button-upload" type="button" className="btn btn-default">
+                      Upload <span className="caret"></span>
+                    </button>
+              </Col>
+            </FormGroup>
+            <FormGroup check row>
+            <Col sm={12}>
+              <Button color="danger" size="lg" block>Submit</Button>
+            </Col>
+          </FormGroup>
+        </Form>
+        </div>
+     );
+  }
 
-          <FormGroup row>
-            <Label for="examplePrice" sm={2}>PRICE</Label>
-            <Col sm={10}>
-              <Input type="Price" name="Price"/>
-            </Col>
-          </FormGroup>
 
-          <FormGroup row>
-            <Label for="examplePhoto" sm={2}>PHOTO</Label>
-            <Col sm={10}>
-              <Input type="Photo" name="Photo"/>
-            </Col>
-          </FormGroup>
-          <FormGroup check row>
-          <Col sm={{ size: 10, offset: 6 }}>
-            <Button color="danger">Submit</Button>{' '}
-          </Col>
-        </FormGroup>
-      </Form>
-      </div>
-  )
+}
 export default addFood
--- src/page/login.jsx
+++ src/page/login.jsx
@@ -1,25 +1,81 @@
 import React, { Component} from "react";
-import { Container, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
-import { Link, Route} from "react-router-dom";
+import {
+  Container,
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  FormText } from 'reactstrap';
+import {
+  Link,
+  Route} from "react-router-dom";
 
-const Login = () => (
-  <h1>
-  hello login
-  </h1>
-    // <div className="Container">
-    //   <Form>
-    //      <FormGroup>
-    //        <Label for="exampleEmail">User Name</Label>
-    //        <Input type="text" name="userid" placeholder="Insert Your User Name" />
-    //      </FormGroup>
-    //     <FormGroup>
-    //       <Label for="examplePassword">Password</Label>
-    //       <Input type="password" name="password" placeholder="Insert Your Password" />
-    //     </FormGroup>
-    //     <Button type="button" onClick="check(this.form)" value="login">Submit</Button>
-    //     </Form>
-    // </div>
-);
+
+class Login extends Component{
+  constructor(){
+    super();
+    this.state={
+      username: "",
+      password: "",
+    };
+    this.handleChangeUserName = this.handleChangeUserName.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this)
+  }
+
+  handleChangeUserName(event){
+    let value = event.target.value;
+    this.setState(() =>{
+      return { username: value};
+    })
+  }
+
+  handleChangePassword(event){
+    let value = event.target.value;
+    this.setState(() =>{
+     return{ password: value};
+    })
+  }
+
+  submitForm(event){
+    event.preventDefault()
+  }
+
+
+   render(){
+     console.log('state', this.state);
+     return(
+       <div className="Container" onSubmit={this.submitForm}>
+         <Form>
+            <FormGroup>
+              <Label for="exampleEmail">User Name</Label>
+              <Input
+              type="text"
+              name="userid"
+              placeholder="Insert Your User Name"
+              value={this.state.username}
+              onChange={this.handleChangeUserName}/>
+            </FormGroup>
+           <FormGroup>
+             <Label for="examplePassword">Password</Label>
+             <Input
+             type="password"
+             name="password"
+             placeholder="Insert Your Password"
+             value={this.state.password}
+             onChange={this.handleChangePassword}/>
+           </FormGroup>
+           <FormGroup>
+              <Button outline color="primary" size="lg" block>Sign Up</Button>
+           </FormGroup>
+              <Button outline color="danger" size="lg" block>Submit</Button>
+           </Form>
+       </div>
+     )
+   }
+ }
+
+
 
 
 export default Login
