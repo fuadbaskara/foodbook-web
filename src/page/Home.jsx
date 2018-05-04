912aa2ab398fee398384adcb34eb0c809aa68d92
Miftah Faris <miftah.faris07@yahoo.co.id>
2018-05-04 14:54:25 +0700

    Add get data from api

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import {
   Row,
   Col,
@@ -6,171 +6,56 @@ import {
   CardText,
   CardBody,
   CardTitle,
-  CardSubtitle,
   Button
 } from "reactstrap";
+import axios from "axios";
 import { Link } from "react-router-dom";
 
-const Home = () => (
-  <div className="margin-top-100">
-    <Row className="homeProduct">
-      <Col sm="4">
-        <CardImg
-          top
-          width="25%"
-          src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-          alt="Card image cap"
-        />
-        <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </CardText>
-          <Link className="textNavBar text-right mx-2" to="/food">
-            <Button>Button</Button>
-          </Link>
-        </CardBody>
-      </Col>
-      <Col sm="4">
-        <CardImg
-          top
-          width="25%"
-          src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-          alt="Card image cap"
-        />
-        <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Col>
-      <Col sm="4">
-        <CardImg
-          top
-          width="25%"
-          src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-          alt="Card image cap"
-        />
-        <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Col>
-      <Col sm="4">
-        <CardImg
-          top
-          width="25%"
-          src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-          alt="Card image cap"
-        />
-        <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Col>
-      <Col sm="4">
-        <CardImg
-          top
-          width="25%"
-          src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-          alt="Card image cap"
-        />
-        <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Col>
-      <Col sm="4">
-        <CardImg
-          top
-          width="25%"
-          src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-          alt="Card image cap"
-        />
-        <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Col>
-      <Col sm="4">
-        <CardImg
-          top
-          width="25%"
-          src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-          alt="Card image cap"
-        />
-        <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Col>
-      <Col sm="4">
-        <CardImg
-          top
-          width="25%"
-          src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-          alt="Card image cap"
-        />
-        <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Col>
-      <Col sm="4">
-        <CardImg
-          top
-          width="25%"
-          src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-          alt="Card image cap"
-        />
-        <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Col>
-    </Row>
-  </div>
-);
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3030";
+
+class Home extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      foods: []
+    };
+  }
+
+  async getData() {
+    let foods = "";
+    await axios.get(`${API_URL}/foods`).then(res => {
+      foods = res.data;
+    });
+    await this.setState({ foods: foods });
+
+    console.log(this.state.foods);
+  }
+
+  componentWillMount() {
+    this.getData();
+  }
+
+  render() {
+    return (
+      <div className="margin-top-100">
+        <Row className="homeProduct">
+          {this.state.foods.map((food, index) => (
+            <Col sm="4">
+              <CardImg top width="25%" src={food.photo} alt="Card image cap" />
+              <CardBody>
+                <CardTitle>{food.name}</CardTitle>
+                <CardText />
+                <Link to="/food">
+                  <Button className="width-full">Details</Button>
+                </Link>
+              </CardBody>
+            </Col>
+          ))}
+        </Row>
+      </div>
+    );
+  }
+}
 
 export default Home;
