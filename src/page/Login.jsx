5a0889f401a791dc1902ab2bc2df4866690892dc
stefanus dody <stefanusdody@yahoo.com>
2018-05-07 15:28:13 +0700

    edit login

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -1,48 +1,70 @@
 import React, { Component } from "react";
-import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input } from "reactstrap";
+import axios from "axios";
+
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3030";
 
 class Login extends Component {
   constructor() {
     super();
     this.state = {
-      username: "",
-      password: ""
+      email: "",
+      password: "",
+
     };
-    this.handleChangeUserName = this.handleChangeUserName.bind(this);
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
     this.handleChangePassword = this.handleChangePassword.bind(this);
+    this.submitForm = this.submitForm.bind(this)
   }
 
-  handleChangeUserName(event) {
+  handleChangeEmail(event) {
     let value = event.target.value;
     this.setState(() => {
-      return { username: value };
+      return { email : value };
     });
   }
 
   handleChangePassword(event) {
     let value = event.target.value;
     this.setState(() => {
-      return { password: value };
+      return { password : value };
     });
   }
 
-  submitForm(event) {
-    event.preventDefault();
-  }
+async submitForm(e) {
+  e.preventDefault()
+  console.log(this.state.email, this.state.password)
+  await axios
+    .post(`${API_URL}/accounts/login`, {
+      email: this.state.email,
+      password: this.state.password,
+    })
+    .then(res => {
+      console.log(res.data)
+    })
+    .catch(error => {
+      console.log(error.res);
+    });
+}
 
   render() {
-    console.log("state", this.state);
+    // console.log("state", this.state);
     return (
-      <div className="Container" onSubmit={this.submitForm}>
-        <Form>
+      <div className="Container" >
+        <Form onSubmit={this.submitForm}>
           <FormGroup>
-            <Label for="exampleEmail">User Name</Label>
+            <Label for="exampleEmail">Email</Label>
             <Input
               type="text"
               name="userid"
               placeholder="Insert Your User Name"
-              value={this.state.username}
-              onChange={this.handleChangeUserName}
+              value={this.state.email}
+              onChange={this.handleChangeEmail}
             />
           </FormGroup>
           <FormGroup>
