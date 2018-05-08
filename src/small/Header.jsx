5b174ed16f916d839b99b2a18b20695a42f55253
stefanus dody <stefanusdody@yahoo.com>
2018-05-08 13:50:30 +0700

    edit header

--- src/small/Header.jsx
+++ src/small/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 const StyleImage = {
@@ -8,27 +8,66 @@ const StyleImage = {
   marginTop: "-45px"
 };
 
-const Header = () => {
+class Header extends Component {
+  constructor(){
+    super();
+    this.state ={
+      searchFoods: ""
+    }
+    this.handleChangeSearchFoods = this.handleChangeSearchFoods.bind(this);
+    this.submitForm = this.submitForm.bind(this);
+  }
+
+  handleChangeSearchFoods(event){
+    let value = event.target.value;
+    this.setState(() =>{
+      return { searchFoods : value }
+    })
+  }
+
+  submitForm(event) {
+    event.preventDefault();
+  }
+
+  render() {
+  console.log("state", this.state);
   return (
     <div className="App-header fixed-top">
-      <div className="display-inline margin-right-30">
-        <input type="text" className="inputText border border-danger" />
-        <button className="button-border-sign textNavBar">Search</button>
+      <div className="display-inline margin-right-30"
+       onSubmit={this.submitForm}>
+         <input
+           type="text"
+           className="inputText border border-danger"
+           value={this.state.searchFoods}
+           onChange={this.handleChangeSearchFoods}/>
+         <button
+           className="button-border-sign textNavBar"
+           onClick={this.submitForm}>Search</button>
       </div>
-      <Link className="textNavBar text-right mx-2" to="/">
-        Home
-      </Link>
-      <Link className="textNavBar text-right mx-2" to="/profile">
-        Profile
-      </Link>
-      <Link className="textNavBar text-right mx-2" to="/addfood">
-        Add Food
-      </Link>
-      <Link className="border-sign textNavBar text-right mx-2" to="/login">
-        Sign In
-      </Link>
-      <Link className="border-sign textNavBar text-right mx-2" to="/signup">
-        Sign Up
+        <Link
+          className="textNavBar text-right mx-2"
+          to="/">
+          Home
+        </Link>
+        <Link
+          className="textNavBar text-right mx-2"
+          to="/profile">
+          Profile
+        </Link>
+        <Link
+          className="textNavBar text-right mx-2"
+          to="/addfood">
+          Add Food
+        </Link>
+        <Link
+          className="border-sign textNavBar text-right mx-2"
+          to="/login">
+          Sign In
+        </Link>
+      <Link
+          className="border-sign textNavBar text-right mx-2"
+          to="/signup">
+          Sign Up
       </Link>
       <div>
         <Link to="/">
@@ -41,6 +80,7 @@ const Header = () => {
       </div>
     </div>
   );
-};
+ }
+}
 
 export default Header;
