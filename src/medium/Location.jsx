b9d71ea3ab87ec0e13a34086ca653fd7d67840ac
Miftah Faris <miftah.faris07@yahoo.co.id>
2018-05-08 19:41:45 +0700

    Resolved warning

--- src/medium/Location.jsx
+++ src/medium/Location.jsx
@@ -1,10 +1,6 @@
-import React, { Component } from "react";
+import React from "react";
 
 export default class Locations extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <div>
