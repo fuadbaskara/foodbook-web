bbbb38063d5dd746b1dae7c4a9d95322a1854bcc
Fuad Baskara P <fuadbaskara@gmail.com>
2018-05-09 12:56:12 +0700

    modify Carousels

--- src/medium/Carousels.jsx
+++ src/medium/Carousels.jsx
@@ -7,24 +7,23 @@ import {
   CarouselCaption
 } from "reactstrap";
 
-// const items = [
-//   {
-//     src: "https://cdn.filestackcontent.com/aTIr8zQDCvM7BVQsrmA3",
-//     altText: "Slide 1",
-//     caption: "Slide 1"
-//   },
-//   {
-//     src: "https://cdn.filestackcontent.com/o3aB2kaLS2a9PUmi1zqg",
-//     altText: "Slide 2",
-//     caption: "Slide 2"
-//   },
-//   {
-//     src:
-//       "data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa21%20text%20%7B%20fill%3A%23333%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa21%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23555%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22277%22%20y%3D%22218.3%22%3EThird%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E",
-//     altText: "Slide 3",
-//     caption: "Slide 3"
-//   }
-// ];
+const items = [
+  {
+    src: "",
+    altText: "Slide 1",
+    caption: "Slide 1"
+  },
+  {
+    src: "",
+    altText: "Slide 2",
+    caption: "Slide 2"
+  },
+  {
+    src: "",
+    altText: "Slide 3",
+    caption: "Slide 3"
+  }
+];
 
 class Carousels extends Component {
   constructor(props) {
@@ -41,19 +40,19 @@ class Carousels extends Component {
     this.goToIndex = this.goToIndex.bind(this);
     this.onExiting = this.onExiting.bind(this);
     this.onExited = this.onExited.bind(this);
-    this.getPhotos = this.getPhotos.bind(this);
+    // this.getPhotos = this.getPhotos.bind(this);
   }
 
-  async componentWillMount() {
-    await this.getPhotos();
-    console.log(this.state.photos);
-  }
-
-  async getPhotos() {
-    this.photos = await this.props.photos;
-    this.setState({ photos: this.photos });
-    console.log(this.photos);
-  }
+  // async componentWillMount() {
+  //   await this.getPhotos();
+  //   console.log(this.state.photos);
+  // }
+  //
+  // async getPhotos() {
+  //   this.photos = await this.props.photos;
+  //   this.setState({ photos: this.photos });
+  //   console.log(this.photos);
+  // }
 
   onExiting() {
     this.animating = true;
@@ -66,7 +65,7 @@ class Carousels extends Component {
   next() {
     if (this.animating) return;
     const nextIndex =
-      this.state.activeIndex === this.state.photos.length - 1
+      this.state.activeIndex === items.length - 1
         ? 0
         : this.state.activeIndex + 1;
     this.setState({ activeIndex: nextIndex });
@@ -76,7 +75,7 @@ class Carousels extends Component {
     if (this.animating) return;
     const nextIndex =
       this.state.activeIndex === 0
-        ? this.state.photos.length - 1
+        ? items.length - 1
         : this.state.activeIndex - 1;
     this.setState({ activeIndex: nextIndex });
   }
@@ -93,7 +92,7 @@ class Carousels extends Component {
     //   console.log(this.props.photos);
     // }
 
-    const slides = this.state.photos.map(item => {
+    const slides = items.map(item => {
       return (
         <CarouselItem
           onExiting={this.onExiting}
@@ -116,7 +115,7 @@ class Carousels extends Component {
         previous={this.previous}
       >
         <CarouselIndicators
-          items={this.state.photos}
+          items={items}
           activeIndex={activeIndex}
           onClickHandler={this.goToIndex}
         />
