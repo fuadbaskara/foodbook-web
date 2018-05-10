b47543bb8bc2938f2ed227d714430ae119e268fa
Miftah Faris <miftah.faris07@yahoo.co.id>
2018-05-10 19:55:10 +0700

    Fix name food into title case

--- src/medium/FoodThumbnail.jsx
+++ src/medium/FoodThumbnail.jsx
@@ -16,7 +16,9 @@ const FoodThumbnail = ({ food, index }) => (
   <Col xs={11} sm={5} md={4} lg={3} className="card-full border-red">
     <CardImg height="50%" src={food.photos[0][0]} alt="Food Image" />
     <CardBody>
-      <CardTitle>{_.capitalize(food.name)}</CardTitle>
+      <CardTitle>
+        {_.map(food.name.split(" "), _.upperFirst).join(" ")}
+      </CardTitle>
       {food.overview.length > MAX_LENGTH ? (
         <div>
           {`${food.overview.substring(0, MAX_LENGTH)} `}
